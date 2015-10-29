13e741661c9adcf7adbdb329abedb33f1a3e023c
fwong <fswong@gmail.com>
2015-10-29 12:42:03 -0700

    seeded with customer, best use, category and brand data

--- seed.py
+++ seed.py
@@ -1,10 +1,13 @@
-from model import Customer
+from model import Customer, BestUse, Brand
 from model import connect_to_db, db
 from server import app
 
 
 def load_customers():
+    """Load fake customer data"""
+
     print "Customers"
+    # Delete rows of existing table, if any
     Customer.query.delete()
 
     for row in open("data/customerdata"):
@@ -20,6 +23,41 @@ def load_customers():
     db.session.commit()
 
 
+def load_bestuses():
+    """Load best use categories"""
+
+    print "BestUses"
+    BestUse.query.delete()
+
+    for row in open("data/bestusesdata"):
+        use = row.strip()
+
+        bestuse = BestUse(use_name=use)
+
+        db.session.add(bestuse)
+
+    db.session.commit()
+
+
+def load_brands():
+    """Load brand names"""
+
+    print "Brands"
+    Brand.query.delete()
+
+    for row in open("data/brandsdata"):
+        name = row.strip()
+
+        b = Brand(brand_name=name)
+
+        db.session.add(b)
+
+    db.session.commit()
+
+
+
+
+
 if __name__ == "__main__":
     connect_to_db(app)
 
@@ -28,3 +66,5 @@ if __name__ == "__main__":
 
     # Import different types of data
     load_customers()
+    load_bestuses()
+    load_brands()
