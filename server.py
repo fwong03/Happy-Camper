250cf2fc4d89e896d528d3980a32f8f211a5f58e
fwong <fswong@gmail.com>
2015-10-29 16:28:31 -0700

    Made prelim routes

--- server.py
+++ server.py
@@ -2,10 +2,12 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, redirect, request, flash, session 
+from flask import Flask
+# , render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import Customer, Category, BestUse, Product, connect_to_db, db
+from model import connect_to_db, db
+# Customer, Category, BestUse, Brand, Product, Tent
 
 
 app = Flask(__name__)
@@ -20,31 +22,28 @@ app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def index():
-    """Homepage."""
+    """Signed out homepage."""
 
-    return "This is the homepage"
+    return "Page you get to if you don't have an account or are not logged in."
 
+
+@app.route('/createaccount')
+def create_account():
+    """Where new users create an account"""
+
+    return "New users will create an account here."
 
 
 @app.route('/login')
-def user_list():
+def login():
     """User login page."""
 
-    return "This will be the user login page"
-
-
-@app.route('/account')
-def show_login():
-    """User account overview page"""
-
-    return "This will be the user account overview page"
-
+    return "Where a user with an account logs in."
 
 
 @app.route('/handle-login', methods=['POST'])
 def handle_login():
     """Process login form"""
-
     pass
 
     # username = request.form['username']
@@ -64,3 +63,93 @@ def handle_login():
     #     flash("Sorry, this username does not exist!")
     #     return redirect('/')
 
+
+@app.route('/success')
+def success():
+    """Signed in home page."""
+
+    return "Where you get to if you've successfully logged in."
+
+
+@app.route('/list_item')
+def list_item():
+    """List an item page. 
+
+    Routes from signed in homepage, which has a button to List an Item.
+    Routes to item detail page.
+    """
+
+    return "Where you can list an item to rent out."
+
+@app.route('/product/<int:prod_id>')
+def item_detail():
+    """Item detail page.
+
+    Routes either from Search Results page or List Item page.
+    If click on Borrow This, routes to Borrowed version of this page.
+    """
+    # If item is available, show regular product detail page.
+    # If item is not available, show BOROWED version of page.
+    # Allows you to borrow only if Available is True.
+
+    return "Where you can view item details."
+
+
+
+@app.route('/searchresults')
+def show_results():
+    """Search results page.
+
+    Routes from Signed in Home Page.
+    Routes to Item Detail page.
+
+    """
+
+    return "This will be the search results."
+
+
+@app.route('/customers/<int:cust_id>')
+def show_account():
+    """Where users can check out their account details.
+
+    They can also delete listings, rate stuff, get the emails of people
+    renting to or renting from.
+
+    Routes from account link in nav bar. Always accessible.
+    """
+
+    return "This will be the users account detail page."
+
+
+@app.route('/renter_rate')
+def renter_rate():
+    """Rating page for renter.
+
+    Here they can rent the owner and the product.
+    Routes from Account page and routes to Account Page with flashed message
+    thanking them for their rating."""
+
+    return "Here renters can rate the owner and the product they rented."
+
+
+@app.route('/owner_rate')
+def owner_rate():
+    """Rating page for owner.
+
+    Owners rate renters. They access this page from their account page, and
+    this routes to the account page with a flashed message thanking them."""
+
+    return "Here owners can rate renters."
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the point
+    # that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run()
