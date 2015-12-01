48d633a90fcbefff986304e57081feafaf61114c
fwong <fswong@gmail.com>
2015-11-30 18:43:12 -0800

    ran tests one more time

--- server.py
+++ server.py
@@ -768,11 +768,11 @@ def delist_product():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = False
+    app.debug = True
 
     connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
     app.run()
